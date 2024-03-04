fc4d414f8bda34892b6fb2bcf04304df7f0887f1
Arthur Caetano <arthurcaetano.acrr@gmail.com>
2024-03-04 14:46:14 -0300

    Metodos Customizados

--- Domain/Interfaces/IDespesa/InterfaceDespesa.cs
+++ Domain/Interfaces/IDespesa/InterfaceDespesa.cs
@@ -10,5 +10,8 @@ namespace Domain.Interfaces.IDespesa
 {
     public interface InterfaceDespesa : InterfaceGeneric<Despesa>
     {
+        Task<IList<Despesa>> ListarDespesasUsuario(string emailUsuario);
+
+        Task<IList<Despesa>> ListarDespesasUsuarioNaoPagasMesesAnterior(string emailUsuario);
     }
 }
